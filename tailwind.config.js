6b8ee25a64d8b7612eb7549e96cc85367285259a
sidiahmadou <sidiahmadous@gmail.com>
2022-12-09 16:07:07 +0100

    2nd commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,30 @@ module.exports = {
   theme: {
     extend:
     {
-      fontFamily: {
-        'grotesk': ['Space Grotesk', 'sans-serif']
+      keyframes: {
+        slideDown: {
+          '0%': { transform: 'translateY(-100px)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+        appear:{
+          '0%': { opacity: '0', scale:'0'},
+          '50%': { opacity: '0.5', scale:'0.5' },
+          '100%': { opacity: '1', scale:'1' },
+        },
+        slideRight:{
+          '0%':{opacity:0, transform:'translateX(100px)'},
+          '100%':{opacity:1, transform:'translateX(0)'},
+        }
+      },
+      
+      animation:{
+      'slideDown':'slideDown 1s linear',
+      'appear':'appear 1s linear forwards',
+      'slideRight':'slideRight 2s ease-in-out forwards'
       },
     },
   },
   plugins: [
-    
+  
   ],
 }
